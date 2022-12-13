71673c1e0155e3562e84ac1e2b405fc74cebcfbf
Admin <admin@MacBook-Pro-Admin.local>
2022-12-13 11:18:44 +0200

    update

--- routes/templates/sections/template.section.routes.js
+++ routes/templates/sections/template.section.routes.js
@@ -10,21 +10,21 @@ export const create = async (req, res) => {
         }
 
         const TEMPLATE_PAGE_ID = req.body.templateId;
-        const sect = await Template.findById(TEMPLATE_PAGE_ID);
+        // const sect = await Template.findById(TEMPLATE_PAGE_ID);
 
         console.log('params =>>>>', req.body);
         console.log('sections =>>>>', sect.sections);
 
         // if (req.body.title) {
-        // await Template.updateOne(
-        //     {
-        //         _id: TEMPLATE_PAGE_ID,
-        //     },
-        //     {
-        //         sections: [...sect],
-        //     },
-        // );
-        // }
+        await Template.updateOne(
+            {
+                _id: TEMPLATE_PAGE_ID,
+            },
+            {
+                sections: [...sect.sections, req.body],
+            },
+        );
+        }
 
         // {
         //     id: req.body.id,
