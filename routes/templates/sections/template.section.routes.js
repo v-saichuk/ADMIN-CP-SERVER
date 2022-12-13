d8d612b623914d44226a0459c5de09c957011421
Admin <admin@MacBook-Pro-Admin.local>
2022-12-13 11:19:04 +0200

    update

--- routes/templates/sections/template.section.routes.js
+++ routes/templates/sections/template.section.routes.js
@@ -24,7 +24,7 @@ export const create = async (req, res) => {
                 sections: [...sect.sections, req.body],
             },
         );
-        }
+        // }
 
         // {
         //     id: req.body.id,
